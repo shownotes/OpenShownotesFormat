81424eb2f1c6955a9bc39c8cbe3ab641cd116ff8
Simon Waldherr <git@simon.waldherr.eu>
2012-09-12 15:42:25 +0200

    tiny API and style changes

--- api.php
+++ api.php
@@ -2,7 +2,24 @@
 
 $getpad = $_GET['pad'];
 $mode = $_GET['mode'];
-$dl = $_GET['dl'];
+
+if(isset($_GET['dl']))
+  {
+    $dl = true;
+  }
+else
+  {
+    $dl = false;
+  }
+
+if(isset($_GET['url']))
+  {
+    $url = $_GET['url'];
+  }
+else
+  {
+    $url = '';
+  }
 
 if(($mode != '')&&($getpad != ''))
   {
@@ -37,34 +54,41 @@ if(($mode != '')&&($getpad != ''))
       {
         include "./export/anycast.php";
         header("Content-Type: text/html");
-        header("Content-Disposition: attachment; filename=\"shownotes.html\"");
+        if($dl){header("Content-Disposition: attachment; filename=\"shownotes.html\"");}
         echo osf_export_anycast($shownotes['export']);
       }
     elseif($_GET['mode'] == 'morehtml')
       {
         include "./export/anycast.php";
         header("Content-Type: text/html");
-        header("Content-Disposition: attachment; filename=\"shownotes.html\"");
+        if($dl){header("Content-Disposition: attachment; filename=\"shownotes.html\"");}
         echo osf_export_anycast($shownotes['export'], true);
+      }
+    elseif($_GET['mode'] == 'metaebene')
+      {
+        include "./export/metaebene.php";
+        header("Content-Type: text/html");
+        if($dl){header("Content-Disposition: attachment; filename=\"shownotes.html\"");}
+        echo osf_export_metaebene($shownotes['export'], $url);
       }
     elseif($_GET['mode'] == 'psc')
       {
         include "./export/psc.php";
         header("Content-Type: text/plain");
-        header("Content-Disposition: attachment; filename=\"shownotes.txt\"");
+        if($dl){header("Content-Disposition: attachment; filename=\"shownotes.txt\"");}
         echo osf_export_psc($shownotes['export']);
       }
     elseif($_GET['mode'] == 'chapter')
       {
         include "./export/chapterlist.php";
         header("Content-Type: text/plain");
-        header("Content-Disposition: attachment; filename=\"shownotes.txt\"");
+        if($dl){header("Content-Disposition: attachment; filename=\"shownotes.txt\"");}
         echo osf_export_chapterlist($shownotes['export']);
       }
     else
       {
         header("Content-Type: text/plain");
-        header("Content-Disposition: attachment; filename=\"shownotes.txt\"");
+        if($dl){header("Content-Disposition: attachment; filename=\"shownotes.txt\"");}
         print_r($shownotes);
       }
   }
@@ -72,12 +96,13 @@ elseif($mode == '')
   {
     header("Content-Type: text/html");
     echo '<p>please select export mode: ';
-    echo '<a href="?mode=json&pad='.$getpad.'">JSON</a>, ';
-    echo '<a href="?mode=html&pad='.$getpad.'">HTML</a>, ';
-    echo '<a href="?mode=morehtml&pad='.$getpad.'">HTML (long)</a>, ';
-    echo '<a href="?mode=psc&pad='.$getpad.'">PSC</a>, ';
-    echo '<a href="?mode=chapter&pad='.$getpad.'">Chapterlist</a> or ';
-    echo '<a href="?mode=PHP&pad='.$getpad.'">PHP</a>, ';
+    echo '<a href="?mode=json&pad='.$getpad.'">JSON</a> <a href="?mode=json&dl&pad='.$getpad.'">&lang;DL&rang;</a>, ';
+    echo '<a href="?mode=html&pad='.$getpad.'">anyca.st</a> <a href="?mode=html&dl&pad='.$getpad.'">&lang;DL&rang;</a>, ';
+    echo '<a href="?mode=morehtml&pad='.$getpad.'">anyca.st (long)</a> <a href="?mode=morehtml&dl&pad='.$getpad.'">&lang;DL&rang;</a>, ';
+    echo '<a href="?mode=metaebene&pad='.$getpad.'">metaebene.me (long)</a> <a href="?mode=metaebene&dl&pad='.$getpad.'">&lang;DL&rang;</a>, ';
+    echo '<a href="?mode=psc&pad='.$getpad.'">PSC</a> <a href="?mode=psc&dl&pad='.$getpad.'">&lang;DL&rang;</a>, ';
+    echo '<a href="?mode=chapter&pad='.$getpad.'">Chapterlist</a> <a href="?mode=chapter&dl&pad='.$getpad.'">&lang;DL&rang;</a> or ';
+    echo '<a href="?mode=PHP&pad='.$getpad.'">PHP</a> <a href="?mode=PHP&dl&pad='.$getpad.'">&lang;DL&rang;</a>, ';
   }
 
 ?>
