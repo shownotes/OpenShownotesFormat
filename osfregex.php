45dd511f2692b7f1f76bbd0918c28871326c93fc
Simon Waldherr <git@simon.waldherr.eu>
2012-09-20 20:16:23 +0200

    osf parser now with comments (in german), another example added (mm96)

--- osfregex.php
+++ osfregex.php
@@ -2,38 +2,53 @@
 
 function osf_convert_time($string)
   {
+    // Diese Funktion wandelt Zeitangaben vom Format 01:23:45 (H:i:s) in Sekundenangaben um
     $strarray = explode(':', $string);
     return (($strarray[0]*3600)+($strarray[1]*60)+$strarray[2]);
   }
 
 function osf_parser($shownotes)
   {
-
+    // Diese Funktion ist das Herzstück des OSF-Parsers
+    // zuerst werden die regex-Definitionen zum erkennen von Zeilen, Tags, URLs und subitems definiert
     $pattern['zeilen']    = '/((\d\d:\d\d:\d\d)(\\.\d\d\d)?)*(.+)/';
     $pattern['tags']      = '((#)(\S*))';
     $pattern['urls']      = '(\s+((http(|s)://\S{0,128})\s))';
     $pattern['urls2']     = '(\<((http(|s)://\S{0,128})>))';
     $pattern['kaskade']   = '/^([\t ]*-+ )/';
     
+    // danach werden mittels des zeilen-Patterns die Shownotes in Zeilen/items geteilt
     preg_match_all($pattern['zeilen'], $shownotes, $zeilen, PREG_SET_ORDER);
     
+    // Zählvariablen definieren
+    // i = item, lastroot = Nummer des letzten Hauptitems, kaskadei = Verschachtelungstiefe
     $i = 0;
     $lastroot = 0;
     $kaskadei = 0;
     $returnarray['info']['zeilen'] = 0;
+    
+    // Zeile für Zeile durch die Shownotes gehen
     foreach($zeilen as $zeile)
       {
+        // Alle Daten der letzten Zeile verwerfen
         unset($newarray);
         
+        // Text der Zeile in Variable abspeichern und abschließendes Leerzeichen anhängen
         $text = $zeile[4].' ';
+        
+        // Mittels regex tags und urls extrahieren
         preg_match_all($pattern['tags'],  $text, $tags,  PREG_PATTERN_ORDER);
         preg_match_all($pattern['urls'],  $text, $urls,  PREG_PATTERN_ORDER);
         preg_match_all($pattern['urls2'], $text, $urls2, PREG_PATTERN_ORDER);
         
+        // array mit URLs im format <url> mit array mit URLs im format  url  zusammenführen
         $urls = array_merge($urls[2], $urls2[2]);
         
+        // Zeit und Text in Array zur weitergabe speichern
         $newarray['time'] = $zeile[1];
         $newarray['text'] = $zeile[4];
+        
+        // Wenn Tags vorhanden sind, diese ebenfalls im Array speichern
         if(@count($tags[2])>0)
           {
             $newarray['tags'] = $tags[2];
@@ -42,32 +57,48 @@ function osf_parser($shownotes)
                 $newarray['chapter'] = true;
               }
           }
+        
+        // Wenn URLs vorhanden sind, auch diese im Array speichern
         if(@count($urls)>0)
           {
             $newarray['urls'] = $urls;
           }
         
-        
+        // Wenn Zeile mit "- " beginnt im Ausgabe-Array verschachteln
         if((preg_match($pattern['kaskade'], $zeile[0]))||(!preg_match('/(\d\d:\d\d:\d\d)/', $zeile[0])))
           {
             if(preg_match($pattern['kaskade'], $zeile[0]))
               {
                 $returnarray['export'][$lastroot]['subitems'][$kaskadei] = $newarray;
               }
+            
+            // Verschachtelungstiefe hochzählen
             ++$kaskadei;
           }
+        
+        // Wenn die Zeile keine Verschachtelung darstellt
         else
           {
+            // Daten auf oberster ebene einfügen
             $returnarray['export'][$i] = $newarray;
+            
+            // Nummer des letzten Objekts auf oberster ebene auf akutelle Item Nummer setzen
             $lastroot = $i;
+            
+            // Verschachtelungstiefe auf 0 setzen
             $kaskadei = 0;
           }
+        
+        // Item Nummer hochzählen
         ++$i;
       }
+    
+    // Zusatzinformationen im Array abspeichern (Zeilenzahl, Zeichenlänge und Hash der Shownotes)
     $returnarray['info']['zeilen']  = $i;
     $returnarray['info']['zeichen'] = strlen($shownotes);
     $returnarray['info']['hash']    = md5($shownotes);
     
+    // Rückgabe der geparsten Daten
     return $returnarray;
   }
 
