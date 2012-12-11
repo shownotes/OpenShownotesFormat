e1a7fa6a9dbc246153db4b5f4dcc538ff4d9598d
Simon Waldherr <git@simon.waldherr.eu>
2012-12-11 20:22:00 +0100

    cleaned for new Parser-Suite

---
+++ export/modullist.conf.php
@@ -0,0 +1,15 @@
+osf_export_anycast($array, $full=false, $filtertags=array(0 => 'spoiler'))
+osf_export_wikigeeks($array, $full=false, $filtertags=array(0 => 'spoiler'))
+osf_export_chapterlist($array)
+osf_export_psc($array)
+
+
+<?php
+
+$exportmodules = array('anycast'   => array()
+                      ,'wikigeeks' => array()
+                      ,'chapter'   => array()
+                      ,'psc'       => array()
+                      );
+
+?>
